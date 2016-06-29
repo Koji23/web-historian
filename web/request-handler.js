b744aaa9644ef0bb43cf323892ff891f90dc1f44
Koji23 <jordanchong23@gmail.com>
2016-06-29 15:54:50 -0700

    Setup request handler logic

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,23 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');  //\
+var fs = require('fs');
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+
+  var statusCode; 
+  
+  if (req.method === 'GET') {
+    statusCode = 200;
+  } else if (req.method === 'POST') {
+    statusCode = 201;
+  } else if (req.method === 'OPTIONS') {
+    statusCode = 200;
+  }
+
+  res.writeHead(statusCode, httpHelpers.headers);
+
   res.end(archive.paths.list);
 };
